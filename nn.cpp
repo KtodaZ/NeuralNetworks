5a677e2876c959de4d501fad6e507aa44df8ff7a
sblattj <sblattj@gmail.com>
2017-04-21 22:22:41 -0700

    best so far

--- nn.cpp
+++ nn.cpp
@@ -23,11 +23,11 @@ using namespace std;
 #define LearningRate 0.5     /* most books suggest 0.3                        */
 #define Criteria     0.006   /* all outputs must be within this to terminate  */
 #define TestCriteria 0.15     /* all outputs must be within this to generalize */
-#define MaxIterate   1000000  /* maximum number of iterations                */
+#define MaxIterate   100000  /* maximum number of iterations                */
 #define ReportIntv   101     /* print report every time this many cases done*/
 #define Momentum     0.85     /* momentum constant                             */
-#define TrainCases   47      /* number of training cases        */
-#define TestCases    11       /* number of test cases            */
+#define TrainCases   48      /* number of training cases        */
+#define TestCases    10       /* number of test cases            */
 // network topology by column ------------------------------------
 #define NumNodes1    4       /* col 1 - must equal NumINs+1     */
 #define NumNodes2    40       /* col 2 - hidden layer 1, etc.    */
